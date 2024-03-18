e381c3c69ee715ef211ecac90f544bddbeb1ea62
Jakub Szczepaniak <szczepaniak.jkb@gmail.com>
2024-03-18 23:25:49 +0100

    Add conversion from OpCode to byte

--- src/chunk.rs
+++ src/chunk.rs
@@ -12,6 +12,12 @@ impl From<u8> for OpCode {
     }
 }
 
+impl From<OpCode> for u8 {
+    fn from(code: OpCode) -> Self {
+        code as u8
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,5 +27,11 @@ mod tests {
     #[case::opcode_return(0, OpCode::OpReturn)]
     fn test_opcode_from_u8(#[case] from: u8, #[case] expected: OpCode) {
         assert_eq!(OpCode::from(from), expected)
+    }
+
+    #[rstest]
+    #[case::opcode_return(OpCode::OpReturn, 0)]
+    fn test_byte_from_opcode(#[case] from: OpCode, #[case] expected: u8) {
+        assert_eq!(u8::from(from), expected)
     }
 }
