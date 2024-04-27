7da29cfc73b08dc185da7e230de66637c31fe8df
Jakub Szczepaniak <jakub.szczepaniak@heyjobs.de>
2024-04-27 22:19:14 +0200

    WIP: Move to storing constants in the separate ValueArray struct.

--- src/chunk.rs
+++ src/chunk.rs
@@ -4,7 +4,7 @@ use std::fmt::Display;
 use std::io::Write;
 #[derive(Debug, PartialEq, Clone)]
 pub enum OpCode {
-    Constant(Value),
+    Constant,
     Negate,
     Return,
     Add,
@@ -18,31 +18,11 @@ impl Display for OpCode {
         write!(f, "OpCode")
     }
 }
-pub struct ChunkEntry {
-    code: OpCode,
-    line: usize,
-}
-
-impl Display for ChunkEntry {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.code {
-            OpCode::Return => Ok(()),
-            OpCode::Constant(val) => write!(f, "{}", val),
-            OpCode::Negate => Ok(()),
-            _ => Ok(()),
-        }
-    }
-}
-
-impl ChunkEntry {
-    pub fn new(code: OpCode, line: usize) -> Self {
-        Self { code, line }
-    }
-}
 
 pub struct Chunk {
-    code: Vec<ChunkEntry>,
-    lines: Vec<(usize, usize)>,
+    code: Vec<u8>,
+    lines: Vec<usize>,
+    constants: ValueArray
 }
 
 impl Chunk {
@@ -50,48 +30,34 @@ impl Chunk {
         Self {
             code: Vec::new(),
             lines: Vec::new(),
+            constants: ValueArray::new()
         }
     }
 
+    pub fn write(&mut self, byte: u8, line: usize){
+        self.code.push(byte);
+        self.lines.push(line);
+    }
+    
     pub fn write_opcode(&mut self, code: OpCode, line: usize) {
-        self.code.push(ChunkEntry::new(code, line));
-        self.add_line(line)
+        self.code.push(code.into());
+        self.lines.push(line)
     }
 
-    pub fn add_constant(&mut self, value: Value, line: usize) {
-        let constant = OpCode::Constant(value);
-        let chunk_entry = ChunkEntry {
-            code: constant,
-            line,
-        };
-        self.code.push(chunk_entry);
-        self.add_line(line)
+    pub fn add_constant(&mut self, value: Value) -> Option<u8>{
+        let index = self.constants.write(value);
+        u8::try_from(index).ok()
     }
 
-    fn add_line(&mut self, line_number: usize) {
-        if let Some(last) = self.lines.last_mut() {
-            if last.1 == line_number {
-                last.0 += 1;
-                return;
-            }
-        }
-        self.lines.push((1, line_number))
+    pub fn get_constant(&self, index: usize) -> Value {
+        self.constants.read_at(index)
     }
-    fn get_line(&self, index: usize) -> Option<usize> {
-        let mut total_len: usize = 0;
-        for (len, value) in &self.lines {
-            total_len += *len;
-            if total_len > index {
-                return Some(*value);
-            }
-        }
-        None
-    }
+
     pub fn free(&mut self) {
         self.code = Vec::new();
         self.lines = Vec::new();
+        self.constants.free();
     }
-
     pub fn disassemble(&self, chunk_name: &str, output: &mut impl Write) {
         writeln!(output, "=={}==", chunk_name).unwrap();
 
@@ -103,21 +69,21 @@ impl Chunk {
     pub fn disassemble_instruction(&self, offset: usize, output: &mut impl Write) -> usize {
         write!(output, "{offset:04}").unwrap();
 
-        if offset > 0 && self.get_line(offset) == self.get_line(offset - 1) {
+        if offset > 0 && self.lines[offset] == self.lines[offset - 1] {
             write!(output, "    | ").unwrap();
         } else {
-            write!(output, " {:4} ", self.get_line(offset).unwrap()).unwrap();
+            write!(output, " {:4} ", self.lines[offset]).unwrap();
         }
-        let instruction = &self.code[offset];
-        match instruction.code {
+        let instruction = &self.code[offset].into();
+        match instruction {
             OpCode::Return => self.simple_instruction("OP_RETURN", offset, output),
             OpCode::Negate => self.simple_instruction("OP_NEGATE", offset, output),
             OpCode::Add => self.simple_instruction("OP_ADD", offset, output),
             OpCode::Substract => self.simple_instruction("OP_SUBSTRACT", offset, output),
             OpCode::Multiply => self.simple_instruction("OP_MULTIPLY", offset, output),
             OpCode::Divide => self.simple_instruction("OP_DIVIDE", offset, output),
-            OpCode::Constant(value) => {
-                self.constant_instruction("OP_CONSTANT", offset, value, output)
+            OpCode::Constant => {
+                self.constant_instruction("OP_CONSTANT", offset, output)
             }
         }
     }
@@ -131,18 +97,16 @@ impl Chunk {
         &self,
         name: &str,
         offset: usize,
-        value: Value,
         output: &mut impl Write,
     ) -> usize {
+        let constant_index = self.code[offset+1];
+        let value = self.constants.read_at(constant_index as usize);
         write!(output, "{name:-16} {offset:4} '").unwrap();
         write!(output, "{value}").unwrap();
         writeln!(output, "'").unwrap();
         offset + 2
     }
 
-    pub fn read(&self, index: usize) -> OpCode {
-        self.code[index].code.clone()
-    }
 }
 
 impl Emmitable for Chunk {
@@ -153,6 +117,11 @@ impl Emmitable for Chunk {
         self.write_opcode(OpCode::from(byte1), line);
         self.add_constant(byte2 as Value, line);
     }
+
+    fn add_constant(&mut self, val: Value, line: usize) {
+        
+    }
+
 }
 
 
@@ -165,7 +134,7 @@ impl OpCodable for Chunk {
 impl From<u8> for OpCode {
     fn from(value: u8) -> Self {
         match value {
-            0 => Self::Constant(0.0),
+            0 => Self::Constant,
             1 => Self::Return,
             2 => Self::Negate,
             3 => Self::Add,
@@ -173,6 +142,20 @@ impl From<u8> for OpCode {
             5 => Self::Multiply,
             6 => Self::Divide,
             _ => todo!("Undefined opcode conversion!"),
+        }
+    }
+}
+
+impl From<OpCode> for u8 {
+    fn from(value: OpCode) -> Self {
+        match value {
+            OpCode::Constant => 0,
+            OpCode::Return => 1,
+            OpCode::Negate => 2, 
+            OpCode::Add => 3, 
+            OpCode::Substract => 4, 
+            OpCode::Multiply => 5, 
+            OpCode::Divide => 6
         }
     }
 }
@@ -192,7 +175,7 @@ mod tests {
     #[rstest]
     fn test_write_constant_to_chunk() {
         let mut chunk = Chunk::new();
-        chunk.add_constant(1.2, 44);
+        chunk.add_constant(1.2);
         assert_eq!(chunk.code.len(), 1)
     }
 
@@ -200,7 +183,7 @@ mod tests {
     fn test_free_the_chunk() {
         let mut chunk = Chunk::new();
         chunk.write_opcode(OpCode::Return, 23);
-        chunk.add_constant(1.2, 43);
+        chunk.add_constant(1.2);
         chunk.free();
         assert_eq!(chunk.code.len(), 0)
     }
@@ -226,7 +209,7 @@ mod tests {
     fn test_disassemble_chunk_with_const() {
         let mut chunk = Chunk::new();
         let mut output = Vec::new();
-        chunk.add_constant(12.4, 133);
+        chunk.add_constant(12.4);
 
         chunk.disassemble("test chunk", &mut output);
 
--- src/value.rs
+++ src/value.rs
@@ -1,1 +1,33 @@
 pub type Value = f64;
+
+
+pub struct ValueArray {
+    values: Vec<Value>
+}
+
+impl ValueArray {
+    pub fn new() -> Self {
+        Self {
+            values: Vec::new()
+        }
+    }
+
+    //returns the index of value that was inserted
+    pub fn write(&mut self, value: Value) -> usize{
+        let count = self.values.len();
+        self.values.push(value);
+        count
+    }
+
+    pub fn free(&mut self) {
+        self.values = Vec::new();
+    }
+
+    pub fn print_at(&self, index: usize) {
+        print!("{}", self.values[index]);
+    }
+
+    pub fn read_at(&self, index: usize) -> Value {
+        self.values[index]
+    }
+}
